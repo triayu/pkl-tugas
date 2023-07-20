6de303bb984d98e6ed156ab047285204a0896562
rahmadas <rahmada365@gmail.com>
2023-07-20 14:38:48 +0800

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,146 @@
+import 'dart:developer';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(
+        title: 'Flutter Demo Home Page',
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: () {},
+          icon: const Icon(Icons.home),
+        ),
+        title: const Text('Vivi '),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          InkWell(
+            onTap: () {
+              print('String');
+            },
+            child: Padding(
+                padding: EdgeInsets.all(30),
+                child: Image.asset(
+                  'img/login.png',
+                )),
+          ),
+          Text(
+            'Email',
+            style: TextStyle(
+              fontSize: 15,
+              fontWeight: FontWeight.bold,
+              color: Colors.black,
+            ),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Container(
+              width: double.infinity,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                color: Colors.white,
+              ),
+              child: TextField(
+                decoration: InputDecoration(
+                    border: InputBorder.none,
+                    hintText: "Masukan Email Anda",
+                    contentPadding:
+                        EdgeInsets.symmetric(horizontal: 16, vertical: 17)),
+              )),
+          Text(
+            'Password',
+            style: TextStyle(
+              fontSize: 15,
+              fontWeight: FontWeight.bold,
+              color: Colors.black,
+            ),
+            textAlign: TextAlign.left,
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Container(
+              width: double.infinity,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                color: Colors.white,
+              ),
+              child: TextField(
+                decoration: InputDecoration(
+                    border: InputBorder.none,
+                    hintText: "Masukan Password Anda",
+                    contentPadding:
+                        EdgeInsets.symmetric(horizontal: 16, vertical: 17)),
+              )),
+          Text(
+            '*Password 8 karakter dan Huruf Besar',
+            style: TextStyle(color: Colors.amber, fontSize: 7),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+
+  // Container(
+  //             padding: EdgeInsets.all(10),
+  //             margin: EdgeInsets.all(10),
+  //             decoration: BoxDecoration(
+  //               borderRadius: BorderRadius.circular(20),
+  //               color: Colors.green[600],
+  //             ),
+  //             child: Text(
+  //               'Finsh daily goal',
+  //               style: TextStyle(fontSize: 15, color: Colors.white),
+  //               textAlign: TextAlign.center,
+  //             )),
